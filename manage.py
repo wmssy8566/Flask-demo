a874448eff5b2c928c58520862a8a18614268081
wmssy8566 <wmssy8566@126.com>
2017-11-15 22:53:49 +0800

    更新到8.4
    更新到8.4

--- manage.py
+++ manage.py
@@ -14,6 +14,7 @@ def make_shell_context():
 manager.add_command("shell", Shell(make_context = make_shell_context()))
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
 def test():
 	import unittest
